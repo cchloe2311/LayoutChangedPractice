bab4116c75b181bedeabd43bb90c9b845707e4cb
최서정 <cchloe2311@daum.net>
2021-03-05 14:32:21 +0900

    added comments

--- app/src/main/java/com/example/layoutchangedpractice/MainActivity.kt
+++ app/src/main/java/com/example/layoutchangedpractice/MainActivity.kt
@@ -9,6 +9,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * View Tree 구조
+     * TestLayout
+     * ㄴ TestView(1)
+     * ㄴ TestView(2)
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +22,16 @@ class MainActivity : AppCompatActivity() {
 
     fun changeSize(v : View) {
         Log.d("MainActivity log", "changeSize called")
-        test_view1.layoutParams = LinearLayout.LayoutParams(400, 400)
+        test_view1.layoutParams = LinearLayout.LayoutParams(400, 400) // TestView(1) size 변경
+        /**
+         * TestLayout onMeasure called
+         * TestView(1) onMeasure called
+         * TestLayout onMeasure called
+         * TestView(2) onMeasure called
+         * TestLayout onMeasure called
+         * TestLayout onMeasure called
+         *
+         * 자식뷰인 TestView(1)의 사이즈 변경이 TestLayout, TestView(2)에도 영향을 끼침
+         */
     }
 }
